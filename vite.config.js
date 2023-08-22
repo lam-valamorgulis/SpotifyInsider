129b24eef8f3a1748533b954bf6991fa2f985c49
lam-valamorgulis <80387108+lam-valamorgulis@users.noreply.github.com>
2023-08-22 16:50:15 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -34,4 +34,17 @@ export default defineConfig({
     }),
   ],
   plugins: [react(), commonjs()],
+  resolve: {
+  alias: {
+    process: "process/browser",
+    buffer: "buffer",
+    crypto: "crypto-browserify",
+    stream: "stream-browserify",
+    assert: "assert",
+    http: "stream-http",
+    https: "https-browserify",
+    os: "os-browserify",
+    url: "url",
+    util: "util",
+  },
 });
