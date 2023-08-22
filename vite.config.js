20efcae930934eddbcb2baade149eaf773839bf5
lam-valamorgulis <80387108+lam-valamorgulis@users.noreply.github.com>
2023-08-22 16:14:14 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -79,29 +79,29 @@ export default {
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
-  optimizeDeps: {
-        esbuildOptions: {
-            // Node.js global to browser globalThis
-            define: {
-                global: 'globalThis'
-            },
-            // Enable esbuild polyfill plugins
-            plugins: [
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true
-                })
-            ]
-        }
-    }
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//   },
+//   resolve: {
+//     alias: {
+//       './runtimeConfig': './runtimeConfig.browser',
+//     },
+//   },
+//   optimizeDeps: {
+//         esbuildOptions: {
+//             // Node.js global to browser globalThis
+//             define: {
+//                 global: 'globalThis'
+//             },
+//             // Enable esbuild polyfill plugins
+//             plugins: [
+//                 NodeGlobalsPolyfillPlugin({
+//                     buffer: true
+//                 })
+//             ]
+//         }
+//     }
 
-})
+// })
