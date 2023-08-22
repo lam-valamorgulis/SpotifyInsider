0637d753931166ee8fa2c2d27a19cc9720fae28c
lam-valamorgulis <80387108+lam-valamorgulis@users.noreply.github.com>
2023-08-22 21:55:09 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -47,7 +47,7 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 // yarn add --dev @esbuild-plugins/node-modules-polyfill
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 // You don't need to add this to deps, it's included by @esbuild-plugins/node-modules-polyfill
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+// import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 // @ts-ignore
 import path from 'path';
 // @ts-ignore
@@ -64,46 +64,46 @@ export default defineConfig({
     react(),
     rollupNodePolyFill(),
   ],
-  resolve: {
-    alias: {
-      // This Rollup aliases are extracted from @esbuild-plugins/node-modules-polyfill,
-      // see https://github.com/remorses/esbuild-plugins/blob/master/node-modules-polyfill/src/polyfills.ts
-      // process and buffer are excluded because already managed
-      // by node-globals-polyfill
-      util: 'util/util',
+  // resolve: {
+  //   alias: {
+  //     // This Rollup aliases are extracted from @esbuild-plugins/node-modules-polyfill,
+  //     // see https://github.com/remorses/esbuild-plugins/blob/master/node-modules-polyfill/src/polyfills.ts
+  //     // process and buffer are excluded because already managed
+  //     // by node-globals-polyfill
+  //     util: 'util/util',
  
-      sys: 'rollup-plugin-node-polyfills/polyfills/util',
-      events: 'rollup-plugin-node-polyfills/polyfills/events',
-      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-      path: 'rollup-plugin-node-polyfills/polyfills/path',
-      querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
-      punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
-      url: 'rollup-plugin-node-polyfills/polyfills/url',
-      // string_decoder: 'rollup-plugin-node-polyfills/polyfills/string-decoder', // throwing me an error
-      http: 'rollup-plugin-node-polyfills/polyfills/http',
-      https: 'rollup-plugin-node-polyfills/polyfills/http',
-      os: 'rollup-plugin-node-polyfills/polyfills/os',
-      assert: 'rollup-plugin-node-polyfills/polyfills/assert',
-      constants: 'rollup-plugin-node-polyfills/polyfills/constants',
-      _stream_duplex:
-        'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
-      _stream_passthrough:
-        'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
-      _stream_readable:
-        'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
-      _stream_writable:
-        'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
-      _stream_transform:
-        'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
-      timers: 'rollup-plugin-node-polyfills/polyfills/timers',
-      console: 'rollup-plugin-node-polyfills/polyfills/console',
-      vm: 'rollup-plugin-node-polyfills/polyfills/vm',
-      zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
-      tty: 'rollup-plugin-node-polyfills/polyfills/tty',
-      domain: 'rollup-plugin-node-polyfills/polyfills/domain',
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  //     sys: 'rollup-plugin-node-polyfills/polyfills/util',
+  //     events: 'rollup-plugin-node-polyfills/polyfills/events',
+  //     stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+  //     path: 'rollup-plugin-node-polyfills/polyfills/path',
+  //     querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
+  //     punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
+  //     url: 'rollup-plugin-node-polyfills/polyfills/url',
+  //     // string_decoder: 'rollup-plugin-node-polyfills/polyfills/string-decoder', // throwing me an error
+  //     http: 'rollup-plugin-node-polyfills/polyfills/http',
+  //     https: 'rollup-plugin-node-polyfills/polyfills/http',
+  //     os: 'rollup-plugin-node-polyfills/polyfills/os',
+  //     assert: 'rollup-plugin-node-polyfills/polyfills/assert',
+  //     constants: 'rollup-plugin-node-polyfills/polyfills/constants',
+  //     _stream_duplex:
+  //       'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+  //     _stream_passthrough:
+  //       'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
+  //     _stream_readable:
+  //       'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
+  //     _stream_writable:
+  //       'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
+  //     _stream_transform:
+  //       'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+  //     timers: 'rollup-plugin-node-polyfills/polyfills/timers',
+  //     console: 'rollup-plugin-node-polyfills/polyfills/console',
+  //     vm: 'rollup-plugin-node-polyfills/polyfills/vm',
+  //     zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+  //     tty: 'rollup-plugin-node-polyfills/polyfills/tty',
+  //     domain: 'rollup-plugin-node-polyfills/polyfills/domain',
+  //     '@': path.resolve(__dirname, './src'),
+  //   },
+  // },
   optimizeDeps: {
     esbuildOptions: {
       plugins: [
@@ -118,10 +118,10 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     plugins: [rollupNodePolyFill()],
+  //   },
+  // },
 });
 
